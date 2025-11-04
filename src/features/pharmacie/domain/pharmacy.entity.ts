6a7481de4a63e920d9bc325cb7e897a524c4c95a
ArnaudBay <arnaudbayale@gmail.com>
2025-11-04 15:48:35 +0100

    feature pharmacie implemente

---
+++ src/features/pharmacie/domain/pharmacy.entity.ts
@@ -0,0 +1,18 @@
+export interface Medicine {
+    
+	id: string;
+	name: string;             
+	category: string;         
+	price: number;           
+	stock: number;            
+	expirationDate: string;   
+	description?: string;
+}
+
+export interface PharmacyRepository {
+	create(medicine: Medicine): Promise<Medicine>;
+	findById(id: string): Promise<Medicine | null>;
+	findAll(): Promise<Medicine[]>;
+	update(medicine: Medicine): Promise<Medicine>;
+	delete(id: string): Promise<void>;
+}
